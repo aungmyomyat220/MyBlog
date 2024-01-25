f4153335dede9e8a993a1979e1c90a096622be1a
aungmyomyat <aungmyomyat874@gmail.com>
2024-01-25 22:30:01 +0630

    small update

---
+++ middleware/validateAPIKey.js
@@ -0,0 +1,11 @@
+const apiKey = process.env.API_KEY;
+function validateApiKey(req, res, next) {
+    const apiKeyHeader = req.headers['x-api-key'];
+    if (apiKeyHeader === apiKey) {
+        next();
+    } else {
+        res.status(401).json({ error: 'Unauthorized' });
+    }
+}
+
+module.exports = validateApiKey
