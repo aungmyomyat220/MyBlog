f74aa4e142e4a1eeefe9441361df02cb3dc52566
aungmyomyat220 <aungmyomyat874@gmail.com>
2024-01-31 22:28:52 +0630

    update

--- middleware/validateAPIKey.js
+++ middleware/validateAPIKey.js
@@ -1,4 +1,4 @@
-const apiKey = "927e0f9a-4451-4210-8dd1-eb47f8ca9089";
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 function validateApiKey(req, res, next) {
     const apiKeyHeader = req.get('API_KEY');
     if (apiKeyHeader === apiKey) {
@@ -7,5 +7,4 @@ function validateApiKey(req, res, next) {
         res.status(401).json({ error: 'Unauthorized' });
     }
 }
-
 module.exports = validateApiKey
