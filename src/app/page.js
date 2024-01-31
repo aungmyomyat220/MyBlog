e2db6a4b641e89e03d6b3354e2c1a87122b43c05
aungmyomyat220 <aungmyomyat874@gmail.com>
2024-01-31 22:39:44 +0630

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 
 export default function Home1() {
     return(
-        <div className="flex w-full h-screen justify-center items-center text-lime-400">
-            <div className="text-5xl">Hello Welcome</div>
-            <div>This is My Blog Server</div>
+        <div className="flex flex-col w-full h-screen justify-center items-center text-lime-400">
+            <div className="text-5xl mb-5">Hello Welcome</div>
+            <div className="text-xl">This is My Blog Server</div>
         </div>
     )
 }
