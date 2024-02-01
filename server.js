a9b1d2906d44b78a1914a9139616c157e3cc4bcb
aungmyomyat220 <aungmyomyat874@gmail.com>
2024-02-01 11:27:40 +0630

    server update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require('body-parser');
-const validateAPIKey = require("./middleware/validateAPIKey");
+// const validateAPIKey = require("./middleware/validateAPIKey");
 const bcrypt = require("bcrypt");
 const {User,Post} = require('./db/mongo')
 const passwordHash = require('./middleware/passwordHash')
