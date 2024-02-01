db16959fcff530371f160000c1e4bc3ad4b043a0
aungmyomyat220 <aungmyomyat874@gmail.com>
2024-02-01 12:55:32 +0630

    server update

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
-
 export default function Home1() {
-    return(
-        <div className="flex flex-col w-full h-screen justify-center items-center text-lime-400">
-            <div className="text-5xl mb-5">Hello Welcome</div>
-            <div className="text-xl">This is My Blog Server</div>
-        </div>
-    )
+  return (
+    <div className="flex flex-col w-full h-screen justify-center items-center text-lime-400">
+      <div className="text-5xl mb-5">Hello Welcome</div>
+      <div className="text-xl">This is My Blog Server</div>
+      <motion.button
+        whileHover={{
+          scale: 1.2,
+          transition: { duration: 1 },
+        }}
+        whileTap={{ scale: 0.9 }}
+      />
+    </div>
+  );
 }
