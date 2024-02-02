f0510d1b75960cdae7baca53d939705fb6e1e793
aungmyomyat220 <aungmyomyat874@gmail.com>
2024-02-03 01:01:44 +0630

    update server

--- db/mongo.js
+++ db/mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const dbURI = "mongodb+srv://aungmyomyat874:aungmyomyat4980@myblog.r74l7i4.mongodb.net/Blogging?retryWrites=true&w=majority";
+const dbURI = process.env.NEXT_PUBLIC_MONGODB_URL;
 mongoose
     .connect(dbURI)
     .then(() => {
--- middleware/validateAPIKey.js
+++ middleware/validateAPIKey.js
@@ -1,10 +1,10 @@
-// const apiKey = process.env.NEXT_PUBLIC_API_KEY;
-// function validateApiKey(req, res, next) {
-//     const apiKeyHeader = req.get('API_KEY');
-//     if (apiKeyHeader === apiKey) {
-//         next();
-//     } else {
-//         res.status(401).json({ error: 'Unauthorized' });
-//     }
-// }
-// module.exports = validateApiKey
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+function validateApiKey(req, res, next) {
+    const apiKeyHeader = req.get('API_KEY');
+    if (apiKeyHeader === apiKey) {
+        next();
+    } else {
+        res.status(401).json({ error: 'Unauthorized' });
+    }
+}
+module.exports = validateApiKey
