c8835908a086461b0c1ad0720aa8e94a19ef1d7f
aungmyomyat220 <aungmyomyat874@gmail.com>
2024-02-04 21:15:18 +0630

    cors middleware update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(sessionMiddleware);
 app.use(bodyParser.json({ limit: '100mb' }));
 app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
-app.use(cors);
+app.use(cors());
 app.use(validateAPIKey);
   
 // Middleware to check authentication
