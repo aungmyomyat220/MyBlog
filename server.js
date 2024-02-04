45c49f64b24c748fb64625caf4272774f7816fc4
aungmyomyat220 <aungmyomyat874@gmail.com>
2024-02-04 20:27:44 +0630

    cors middleware update

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ app.use(cors({
     origin: ['https://myblog-two-lake.vercel.app','http://localhost:3000']
 }));
 app.use(validateAPIKey);
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', ['https://myblog-two-lake.vercel.app','http://localhost:3000']);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
+  
 
 // Middleware to check authentication
 async function authenticate(req, res, next) {
