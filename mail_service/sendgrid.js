b950f3c896b926e39e76ac9efa1e4ef4c49087df
aungmyomyat220 <aungmyomyat874@gmail.com>
2024-02-21 10:09:48 +0630

    update server>> genereate random number

--- mail_service/sendgrid.js
+++ mail_service/sendgrid.js
@@ -1,18 +1,17 @@
 const sgMail = require('@sendgrid/mail')
 const dotenv = require('dotenv');
 dotenv.config();
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-sgMail.setApiKey('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')
-const sixDigits = require('../middleware/genereateSixDigits')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendEmail = async (user) => {
+  console.log("Work");
   const emailData = {
     from: 'aungmyomyat874@gmail.com',
     to: user.userEmail,
     dynamic_template_data: {
       "first_name" : user.userName,
       "verification_link" : "https://myblog-two-lake.vercel.app/confirm",
-      "uuid" : sixDigits
+      "uuid" : '123456'
     },
     template_id: 'd-76a78520ccd04bfd82f84f86713b5edb'
   };
