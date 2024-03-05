295480d157993294b12649666296fbfed31c4ba4
aungmyomyat220 <aungmyomyat874@gmail.com>
2024-03-05 09:31:24 +0630

    add origin

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000','https://myblog-two-lake.vercel.app'],
     credentials: true
   }));
 app.use(session({
